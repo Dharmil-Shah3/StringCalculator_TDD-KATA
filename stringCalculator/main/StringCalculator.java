0f4e7ac2f89a41aacd01d94408c004fc35413940
Dharmil Shah <dhar8866@gmail.com>
2022-08-21 21:40:43 +0530

    TestCase Succeed: emptyStringInput()

--- stringCalculator/main/StringCalculator.java
+++ stringCalculator/main/StringCalculator.java
@@ -3,6 +3,9 @@ package stringCalculator.main;
 public class StringCalculator{
 
     public int add(String inputString){
+        if(inputString.length()==0){
+            return 0;
+        }
         return 1;
     }
 }
