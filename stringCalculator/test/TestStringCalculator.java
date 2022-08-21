db1893ee060e48946b9864f33d687e9ace91a2c4
Dharmil Shah <dhar8866@gmail.com>
2022-08-21 22:00:38 +0530

    TestCase created: singleValueInput() should return value itself

--- stringCalculator/test/TestStringCalculator.java
+++ stringCalculator/test/TestStringCalculator.java
@@ -4,16 +4,31 @@ import stringCalculator.main.StringCalculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.io.InputStream;
+
 public class TestStringCalculator{
 
     StringCalculator calculator = new StringCalculator();
-    String inpuString;
+    String inputString;
     int expected;
 
     @Test
     public void emptyStringInput(){
-        inpuString = "";
+        // single value input should return 0
+        inputString = "";
         expected = 0;
-        assertEquals(expected, calculator.add(inpuString));
+        assertEquals(expected, calculator.add(inputString));
+    }
+
+    @Test
+    public void singleValueInput(){
+        // single value input should return value itself
+        inputString = "4";
+        expected = 4;
+        assertEquals(expected, calculator.add(inputString));
+        
+        inputString = " 36 781 ";
+        expected = 36781;
+        assertEquals(expected, calculator.add(inputString));
     }
 }
