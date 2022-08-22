2a1646ff8a94c0564ddce2b3edec32dd6f6a2ff4
Dharmil Shah <dhar8866@gmail.com>
2022-08-22 14:16:08 +0530

    Refactored StringCalculator.java: Devided add() method into multiple meethods

--- stringCalculator/main/StringCalculator.java
+++ stringCalculator/main/StringCalculator.java
@@ -3,71 +3,33 @@ package stringCalculator.main;
 import java.util.StringTokenizer;
 
 public class StringCalculator{
-
-    public int add(String inputString){
-        
-        inputString = inputString.replace(" ", ""); // Removing whitespaces from string
-
-        String delimiter = ",\n";   // defining default delimiters
-        boolean odd = false;        // for '0//' odd indices's addition
-        boolean even = false;       // for '1//' even indices's addition
-
-        if(inputString.startsWith("0//")){
-            odd = true;
+    
+    private String trimAccordingOddEvenDelimiter(String inputString){
+        char check = inputString.charAt(3);
+        if( Character.isDigit(check) || Character.isLetter(check) ) {
+            // if it's not delimiter then trim '0//' or '1//'
+            return inputString.substring(3);
         }
-        else if(inputString.startsWith("1//")){
-            even = true;
+        else{
+            // if it's delimiter then just trim 0 or 1, keep '//' as is.
+            return inputString.substring(1);
         }
-        if(odd || even){
-            // if '0//' OR '1//' is there so check if next character is delimiter i.e) "0//;\n1;2" or not i.e) "0//1,2,3,4"
-            int check = (int)inputString.charAt(3);
-            if( 
-            ( check>=48 && check<=57) || 
-            ( check>=65 && check<=90) || 
-            ( check>=97 && check<=122)) {
-                // if it's not delimiter then trim '0//' or '1//'
-                inputString = inputString.substring(3);
-            }
-            else{
-                // if it's delimiter then just trim 0 or 1
-                inputString = inputString.substring(1);
-            }
-        }
+    }
 
-        if(inputString.startsWith("//"))
-        {
-            // getting '\n's postion to know end of the delimiter
-            int end = inputString.indexOf("\n");
-            // setting new delimiter including '\n'
-            delimiter += inputString.substring(2, end);
-            // after getting delimiter, remove that part from string to get only numbers
-            inputString = inputString.substring(end+1);
+    private int singleValueEval(String token) throws IllegalArgumentException {
+        if( (int)token.charAt(0) >= 97 && (int)token.charAt(0) <= 122 ){
+            // if number is alphabet then return according values. i.e) a=1...z=26
+            return (int)token.charAt(0) - 96;
         }
-        
-        StringTokenizer tokens = new StringTokenizer(inputString, delimiter);
+        else if(Integer.parseInt(token) < 0){
+            throw new IllegalArgumentException("\n Negative values are not allowed: "+Integer.parseInt(token));
+        }
+        return Integer.parseInt(token);
+    }
 
-        if(inputString.length()==0 || tokens.countTokens()==0 ){
-            // for empty string
-            return 0;
-        }
-        else if(tokens.countTokens() == 1){
-            // for single value
-            String number = tokens.nextToken().replace(" ", "");
-            char firstChar = number.charAt(0);
-            if( (int)firstChar >= 97 && (int)firstChar <= 122 ){
-                // if number is alphabet then return according values. i.e) a=1...z=26
-                return (int)firstChar - 96;
-            }
-            else if(Integer.parseInt(number) < 0){
-                // throws exception if number is negative
-                throw new IllegalArgumentException(
-                "\n Negative values are not allowed: "+Integer.parseInt(inputString));
-            }
-            return Integer.parseInt(inputString);
-        }
-
+    private int multiValueEval(StringTokenizer tokens, boolean odd, boolean even){
         int sum = 0;
-        String negativeNumbers = ""; // to check whether input has any negative values or not
+        String negativeNumbers = "";
         short count=0;  // for '0//' and '1//' (odd and even indices)
 
         while(tokens.hasMoreTokens()){ // for multiple values
@@ -79,19 +41,16 @@ public class StringCalculator{
                 continue;
             }
 
-            // trimmed token and added to 'sum' as an Integer            
-            String number = tokens.nextToken().replace(" ","");
+            String number = tokens.nextToken();
             
             if( (int)number.charAt(0) >= 97 && (int)number.charAt(0) <= 122 ){
-                // if value is an alphabet then convert into according values
+                // if value is an alphabet then convert into according values a=1...z=26
                 sum += (int)number.charAt(0) - 96;
             }
             else if(Integer.parseInt(number) < 0){
-                // if it's negative then add to negativeNumbers String
                 negativeNumbers += Integer.parseInt(number) + ", ";
             }
             else if(Integer.parseInt(number)>1000){
-                // value greater than 1000 should be ingnored
                 continue;
             }
             else{
@@ -102,6 +61,48 @@ public class StringCalculator{
             throw new IllegalArgumentException(
             "\n Negative values are not allowed: "+negativeNumbers);
         }
-        return sum; // returning answer
+        return sum;
+    }
+
+    public int add(String inputString){
+        
+        inputString = inputString.replace(" ", ""); // Removing whitespaces from string
+    
+        String delimiter = ",\n";
+        boolean odd = false;        // for '0//' odd indices's addition
+        boolean even = false;       // for '1//' even indices's addition
+    
+        if(inputString.startsWith("0//")){
+            odd = true;
+        }
+        else if(inputString.startsWith("1//")){
+            even = true;
+        }
+    
+        if(odd || even){
+            // if '0//' OR '1//' is there so check if next character is delimiter i.e) "0//;\n1;2" or not i.e) "0//1,2,3,4"
+            inputString = trimAccordingOddEvenDelimiter(inputString);
+        }
+    
+        if(inputString.startsWith("//"))
+        {
+            // getting '\n's postion to know end of the delimiter
+            int end = inputString.indexOf("\n");
+            delimiter += inputString.substring(2, end);
+            // after getting delimiter, remove that part from string to get only numbers
+            inputString = inputString.substring(end+1);
+        }
+        
+        StringTokenizer tokens = new StringTokenizer(inputString, delimiter);
+    
+        if(inputString.length()==0 || tokens.countTokens()==0 ){ // for empty string
+            return 0;
+        }
+        else if(tokens.countTokens() == 1){ // for single value
+            return singleValueEval(tokens.nextToken());
+        }
+        else{ // for multiple value
+            return multiValueEval(tokens, odd, even);
+        }
     }
 }
