a8017d71013b97bc2c08bd47ba72990373383894
Dharmil Shah <dhar8866@gmail.com>
2022-08-22 05:32:57 +0530

    Refactoring: TestStringCalculator.java

--- stringCalculator/test/TestStringCalculator.java
+++ stringCalculator/test/TestStringCalculator.java
@@ -8,126 +8,81 @@ import static org.junit.Assert.assertThrows;
 public class TestStringCalculator{
 
     StringCalculator calculator = new StringCalculator();
-    String inputString;
-    int expected;
 
     @Test
     public void emptyStringInput(){
         // single value input should return 0
-        inputString = "";
-        expected = 0;
-        assertEquals(expected, calculator.add(inputString));
-        
-        inputString = ", ,, ,,,";
-        assertEquals(expected, calculator.add(inputString));
+        assertEquals(0, calculator.add(" "));
+        assertEquals(0, calculator.add(", ,, ,,,"));
     }
 
     @Test
     public void singleValueInput(){
         // single value input should return value itself
-        inputString = "4";
-        expected = 4;
-        assertEquals(expected, calculator.add(inputString));
-        
-        inputString = " 36 781 ";
-        expected = 36781;
-        assertEquals(expected, calculator.add(inputString));
+        assertEquals(4, calculator.add("4"));
+        assertEquals(36781, calculator.add(" 36 781 "));
     }
 
     @Test
     public void multiValueInputWithDelimiter(){
         // multiple values enterd with delimiter ',' and should return sum of all values
-        inputString = "1,2";
-        expected = 3;
-        assertEquals(expected, calculator.add(inputString));
-
-        inputString = "1,2,3,4, 5";
-        expected = 15;
-        assertEquals(expected, calculator.add(inputString));
+        assertEquals(3, calculator.add("1,2"));
+        assertEquals(15, calculator.add("1,2,3,4, 5"));
     }
 
     @Test
     public void alphabeticValueInput(){
         // a=1, b=2... z=16.   i.e) a+5+b = 8
-        inputString = "1,2,a,c";
-        expected = 7;
-        assertEquals(expected, calculator.add(inputString));
-
-        inputString = "z";
-        expected = 26;
-        assertEquals(expected, calculator.add(inputString));
-
-        inputString = "a,z";
-        expected = 27;
-        assertEquals(expected, calculator.add(inputString));
+        assertEquals(7, calculator.add("1,2,a,c"));
+        assertEquals(26, calculator.add("z"));
+        assertEquals(27, calculator.add("a,z"));
     }
 
     @Test
     public void negativeValueThrowsException(){
         //  Throws exception and display those negative numbers
-        inputString = "-1";
+
         var e = assertThrows(
             IllegalArgumentException.class, 
-            ()->{ calculator.add(inputString); }
+            ()->{ calculator.add("-1"); }
         );
         assertEquals("\n Negative values are not allowed: -1", e.getMessage());
 
-        inputString = "a,-1,8, 69, -7,-79";
         e = assertThrows(
             IllegalArgumentException.class, 
-            ()->{ calculator.add(inputString); }
+            ()->{ calculator.add("a,-1,8, 69, -7,-79"); }
         );
         assertEquals("\n Negative values are not allowed: -1, -7, -79, ", e.getMessage());
     }
 
     @Test
     public void valueGreaterThan1000ShouldBeIgnored(){
-        inputString = "2,1001";
-        expected = 2;
-        assertEquals(expected, calculator.add(inputString));
-
-        inputString = "2022,1234";
-        expected = 0;
-        assertEquals(expected, calculator.add(inputString));
+        assertEquals(2, calculator.add("2,1001"));
+        assertEquals(0, calculator.add("2022,1234"));
     }
 
     @Test
     public void acceptNewlineAsADelimiter(){
         // Accepts '\n' as a delimiter like ','
-        inputString = "1\n2,3";
-        expected = 6;
-        assertEquals(expected, calculator.add(inputString));
-
-        inputString = "50\n500\n5";
-        expected = 555;
+        assertEquals(6, calculator.add("1\n2,3"));
+        assertEquals(555, "50\n500\n5");
     }
 
     @Test
     public void inputWithCustomDelimiter(){
-        inputString = "//;\n1;2";
-        expected = 3;
-        assertEquals(expected, calculator.add(inputString));
-
-        inputString = "//***\n5***5***5";
-        expected = 15;
-        assertEquals(expected, calculator.add(inputString));
+        assertEquals(3, calculator.add("//;\n1;2"));
+        assertEquals(15, calculator.add("//***\n5***5***5"));
     }
 
     @Test
     public void additionOfOddIndices(){
-        inputString = "0//;\n1;2;3,4";
-        expected = 4;
-
-        inputString = "0//***\n6***2,2***4";
-        expected = 8;
+        assertEquals(4, calculator.add("0//;\n1;2;3,4"));
+        assertEquals(8, calculator.add("0//***\n6***2,2***4"));
     }
 
     @Test
     public void additionOfEvenIndices(){
-        inputString = "1//;\n1;2;3,4";
-        expected = 6;
-
-        inputString = "1//***\n6***8,10***12";
-        expected = 20;
+        assertEquals(6, calculator.add("1//;\n1;2;3,4"));
+        assertEquals(20, calculator.add("1//***\n6***8,10***12"));
     }
 }
